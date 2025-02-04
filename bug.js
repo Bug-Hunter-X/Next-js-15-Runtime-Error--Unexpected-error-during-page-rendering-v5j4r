75dfb253a5692041d1d689c1a5448f6e6831dd28
BugHunter-X <askzenai@gmail.com>
2025-02-04 10:30:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,14 @@
+```javascript
+// pages/index.js
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to my Next.js app</h1>
+      <p>This is a paragraph of text.</p>
+    </div>
+  );
+}
+```
+
+This code will produce a runtime error in Next.js 15 if the `pages/index.js` file is missing or contains a syntax error.
